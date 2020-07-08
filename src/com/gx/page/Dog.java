b5c8a277f2429d94a6acfe390d4a1c2ffce67e41
fengfeng521521 <45942616+fengfeng521521@users.noreply.github.com>
2020-07-08 10:24:20 +0800

    加了一个Dog

---
+++ src/com/gx/page/Dog.java
@@ -0,0 +1,17 @@
+package com.gx.page;
+
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
